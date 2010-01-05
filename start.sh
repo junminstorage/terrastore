2a8f072aa2408811a08218f00b157d650279b05b
sergio bossa <sergio.bossa@gmail.com>
2010-01-05 12:13:29 +0100

    Fixed cygwin issue.

--- start.sh
+++ start.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+case "`uname`" in
+  CYGWIN*) cygwin=true ;;
+esac
+
 if [ "${TERRASTORE_HOME}" = "" ]
 then
     export TERRASTORE_HOME=`dirname "$0"`/..
@@ -30,7 +34,14 @@ done
 
 export TC_INSTALL_DIR=${TERRASTORE_HOME}/terrastore-master-libs
 export TC_CONFIG_PATH=$terrastore_master
+
+if [ "$cygwin" != "" ]
+then
+  CLASSPATH=`cygpath --windows --path "$CLASSPATH"`
+  TC_INSTALL_DIR=`cygpath --windows --path "$TC_INSTALL_DIR"`
+fi
+
 . ${TC_INSTALL_DIR}/bin/dso-env.sh -q
 export JAVA_OPTS="$TC_JAVA_OPTS $JAVA_OPTS -XX:+UseParallelGC"
 
-java ${JAVA_OPTS} -cp $CLASSPATH terrastore.startup.Startup $@
+java ${JAVA_OPTS} -classpath "$CLASSPATH" terrastore.startup.Startup "$@"
