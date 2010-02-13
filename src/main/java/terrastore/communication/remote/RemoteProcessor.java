93fdfe6851afe580310dd2a6e991cd654af8cb80
sergio bossa <sergio.bossa@gmail.com>
2010-02-13 16:28:11 +0100

    Minor fix to RemoteProcessor channel options (removed keepalive and address reuse).

--- src/main/java/terrastore/communication/remote/RemoteProcessor.java
+++ src/main/java/terrastore/communication/remote/RemoteProcessor.java
@@ -66,8 +66,6 @@ public class RemoteProcessor extends AbstractSEDAProcessor {
         acceptedChannels = new DefaultChannelGroup(this.toString());
         server = new ServerBootstrap(new NioServerSocketChannelFactory(Executors.newCachedThreadPool(), Executors.newCachedThreadPool()));
         server.setOption("reuseAddress", true);
-        server.setOption("child.keepAlive", true);
-        server.setOption("child.reuseAddress", true);
         server.setPipelineFactory(new ServerChannelPipelineFactory(maxFrameLength, new ServerHandler()));
     }
 
