ebc82888c8bc2eb2dccf61b68bc10c56d39e7c26
sergio bossa <sergio.bossa@gmail.com>
2010-03-18 17:38:29 +0100

    Increased byte buffer size for input reading to 4k.

--- src/main/java/terrastore/util/io/ThreadLocalByteBuffer.java
+++ src/main/java/terrastore/util/io/ThreadLocalByteBuffer.java
@@ -24,7 +24,7 @@ public class ThreadLocalByteBuffer {
 
         @Override
         protected byte[] initialValue() {
-            return new byte[1024];
+            return new byte[1024 * 4];
         }
     };
 
