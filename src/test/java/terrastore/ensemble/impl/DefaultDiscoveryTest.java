c0dd6c0ead484e116bb70d8c8604c76c1692b390
sergio bossa <sergio.bossa@gmail.com>
2010-04-08 20:14:43 +0200

    Fixed view management bugs.

--- src/test/java/terrastore/ensemble/impl/DefaultDiscoveryTest.java
+++ src/test/java/terrastore/ensemble/impl/DefaultDiscoveryTest.java
@@ -5,10 +5,8 @@ import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 import org.easymock.EasyMock;
 import org.junit.Test;
-import terrastore.common.ErrorMessage;
 import terrastore.communication.Cluster;
 import terrastore.communication.Node;
-import terrastore.communication.ProcessingException;
 import terrastore.communication.protocol.MembershipCommand;
 import terrastore.router.Router;
 import static org.easymock.EasyMock.*;
@@ -53,51 +51,6 @@ public class DefaultDiscoveryTest {
         try {
             DefaultDiscovery discovery = new DefaultDiscovery(router, nodeFactory);
             discovery.join(cluster, "localhost:6000");
-            Thread.sleep(3000);
-        } finally {
-            verify(seed, discoveredNode, nodeFactory, router);
-        }
-    }
-
-    @Test
-    public void testJoinAndBootstrapLater() throws Exception {
-        Cluster cluster = new Cluster("cluster", false);
-
-        Node seed = createMock(Node.class);
-        makeThreadSafe(seed, true);
-        seed.connect();
-        expectLastCall().times(2);
-        seed.send(EasyMock.<MembershipCommand>anyObject());
-        expectLastCall().andThrow(new ProcessingException(new ErrorMessage(0, "")));
-        seed.send(EasyMock.<MembershipCommand>anyObject());
-        expectLastCall().andReturn(new View("cluster", Sets.newHashSet(new View.Member("discovered", "localhost", 6000)))).once();
-        seed.disconnect();
-        expectLastCall().times(2);
-        //
-        Node discoveredNode = createMock(Node.class);
-        makeThreadSafe(discoveredNode, true);
-        discoveredNode.connect();
-        expectLastCall().once();
-        //
-        RemoteNodeFactory nodeFactory = createMock(RemoteNodeFactory.class);
-        makeThreadSafe(nodeFactory, true);
-        nodeFactory.makeNode("localhost", 6000, "localhost:6000");
-        expectLastCall().andReturn(seed).once();
-        nodeFactory.makeNode("localhost", 6000, "discovered");
-        expectLastCall().andReturn(discoveredNode).once();
-        //
-        Router router = createMock(Router.class);
-        makeThreadSafe(router, true);
-        router.addRouteTo(cluster, discoveredNode);
-        expectLastCall().once();
-
-        replay(seed, discoveredNode, nodeFactory, router);
-
-        try {
-            DefaultDiscovery discovery = new DefaultDiscovery(router, nodeFactory);
-            discovery.join(cluster, "localhost:6000");
-            Thread.sleep(3000);
-            discovery.schedule(0, 10, TimeUnit.SECONDS);
             Thread.sleep(3000);
         } finally {
             verify(seed, discoveredNode, nodeFactory, router);
@@ -221,7 +174,7 @@ public class DefaultDiscoveryTest {
     }
 
     @Test
-    public void testScheduleRequiresToJoinFirst() throws Exception {
+    public void testScheduleDoesNothingIfNoJoins() throws Exception {
         RemoteNodeFactory nodeFactory = createMock(RemoteNodeFactory.class);
         makeThreadSafe(nodeFactory, true);
         //
