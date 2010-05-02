8babbea33dd3b7a686b55581730e4a23db613ce8
sergio bossa <sergio.bossa@gmail.com>
2010-05-02 19:38:01 +0200

    Configured local jmx agent.

--- src/dist/server/start.sh
+++ src/dist/server/start.sh
@@ -46,6 +46,6 @@ fi
 echo "Starting Terrastore Server ..."
 
 . ${TC_INSTALL_DIR}/bin/dso-env.sh -q
-export JAVA_OPTS="$TC_JAVA_OPTS $JAVA_OPTS -XX:+UseParallelGC -Dlogback.configurationFile=${TERRASTORE_HOME}/terrastore-logback.xml"
+export JAVA_OPTS="$TC_JAVA_OPTS $JAVA_OPTS -XX:+UseParallelGC -Dcom.sun.management.jmxremote -Dlogback.configurationFile=${TERRASTORE_HOME}/terrastore-logback.xml"
 
 java ${JAVA_OPTS} -classpath "$CLASSPATH" terrastore.startup.Startup "$@"
