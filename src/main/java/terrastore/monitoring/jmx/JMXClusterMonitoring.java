0c2925fd22f8caebc32ca40c42d50a88766e8506
sergio bossa <sergio.bossa@gmail.com>
2010-05-02 19:38:41 +0200

    Improved JMX cluster monitoring with auto-update.

--- src/main/java/terrastore/monitoring/jmx/JMXClusterMonitoring.java
+++ src/main/java/terrastore/monitoring/jmx/JMXClusterMonitoring.java
@@ -1,8 +1,16 @@
 package terrastore.monitoring.jmx;
 
+import com.google.common.base.Function;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Sets;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 import javax.management.MBeanServer;
 import javax.management.ObjectName;
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
 import org.apache.commons.lang.builder.ToStringBuilder;
 import terrastore.communication.Cluster;
 import terrastore.communication.Node;
@@ -15,9 +23,11 @@ import terrastore.router.Router;
  */
 public class JMXClusterMonitoring {
 
-    private final static String TERRASTORE_CLUSTER_DOMAIN = "terrastore.monitoring.cluster";
-    private final static String TERRASTORE_CLUSTER_TYPE = "Cluster";
-    private final static String TERRASTORE_NODE_TYPE = "Cluster.Node";
+    private final static String CLUSTER_DOMAIN = "terrastore.monitoring.cluster";
+    private final static String CLUSTER_TYPE = "Cluster";
+    private final static String NODE_TYPE = "Cluster.Node";
+    private final Set<MCluster> clusters = new HashSet<MCluster>();
+    private final Map<String, Set<MNode>> perClusterNodes = new HashMap<String, Set<MNode>>();
     private final MBeanServer mbeanServer;
     private final Router router;
 
@@ -26,37 +36,56 @@ public class JMXClusterMonitoring {
         this.router = router;
     }
 
-    public void update() throws Exception {
-        cleanup();
+    public synchronized void update() throws Exception {
         registerClusters();
-    }
-
-    private void cleanup() throws Exception {
-        for (ObjectName name : mbeanServer.queryNames(new ObjectName(TERRASTORE_CLUSTER_DOMAIN + "*" + ":*"), null)) {
-            mbeanServer.unregisterMBean(name);
-        }
+        registerClusterNodes();
     }
 
     private void registerClusters() throws Exception {
-        for (Cluster cluster : router.getClusters()) {
-            StringBuilder nameBuilder = new StringBuilder(TERRASTORE_CLUSTER_DOMAIN)
-                    .append(":type=").append(TERRASTORE_CLUSTER_TYPE).append(",")
-                    .append("name=").append(cluster.getName());
-            ObjectName name = new ObjectName(nameBuilder.toString());
-            Set<Node> nodes = router.clusterRoute(cluster);
-            String status = nodes.isEmpty() ? MClusterMXBean.Status.UNAVAILABLE.toString() : MClusterMXBean.Status.AVAILABLE.toString();
-            mbeanServer.registerMBean(new MCluster(cluster.getName(), status), name);
-            registerClusterNodes(cluster, nodes);
+        Set<MCluster> currentMClusters = clusters;
+        Set<MCluster> newMClusters = Sets.newHashSet(Iterables.transform(router.getClusters(), new ClusterToMClusterTransformer(router)));
+        for (MCluster candidate : currentMClusters) {
+            if (!newMClusters.contains(candidate)) {
+                StringBuilder objNameBuilder = new StringBuilder(CLUSTER_DOMAIN).append(":type=").append(CLUSTER_TYPE).append(",").append("name=").append(candidate.
+                        getName());
+                ObjectName objName = new ObjectName(objNameBuilder.toString());
+                mbeanServer.unregisterMBean(objName);
+            }
         }
+        for (MCluster candidate : newMClusters) {
+            if (!currentMClusters.contains(candidate)) {
+                StringBuilder objNameBuilder = new StringBuilder(CLUSTER_DOMAIN).append(":type=").append(CLUSTER_TYPE).append(",").append("name=").append(candidate.
+                        getName());
+                ObjectName objName = new ObjectName(objNameBuilder.toString());
+                mbeanServer.registerMBean(candidate, objName);
+            }
+        }
+        clusters.clear();
+        clusters.addAll(newMClusters);
     }
 
-    private void registerClusterNodes(Cluster cluster, Set<Node> nodes) throws Exception {
-        for (Node node : nodes) {
-            StringBuilder nameBuilder = new StringBuilder(TERRASTORE_CLUSTER_DOMAIN).append(".").append(cluster.getName())
-                    .append(":type=").append(TERRASTORE_NODE_TYPE).append(",")
-                    .append("name=").append(node.getName());
-            ObjectName name = new ObjectName(nameBuilder.toString());
-            mbeanServer.registerMBean(new MNode(node.getName(), node.getHost()), name);
+    private void registerClusterNodes() throws Exception {
+        for (Cluster cluster : router.getClusters()) {
+            Set<Node> nodes = router.clusterRoute(cluster);
+            Set<MNode> currentMNodes = perClusterNodes.get(cluster.getName()) != null ? perClusterNodes.get(cluster.getName()) : new HashSet<MNode>(0);
+            Set<MNode> newMNodes = Sets.newHashSet(Iterables.transform(nodes, new NodeToMNodeTransformer()));
+            for (MNode candidate : currentMNodes) {
+                if (!newMNodes.contains(candidate)) {
+                    StringBuilder objNameBuilder = new StringBuilder(CLUSTER_DOMAIN).append(".").append(cluster.getName()).append(":type=").append(NODE_TYPE).
+                            append(",").append("name=").append(candidate.getName());
+                    ObjectName objName = new ObjectName(objNameBuilder.toString());
+                    mbeanServer.unregisterMBean(objName);
+                }
+            }
+            for (MNode candidate : newMNodes) {
+                if (!currentMNodes.contains(candidate)) {
+                    StringBuilder objNameBuilder = new StringBuilder(CLUSTER_DOMAIN).append(".").append(cluster.getName()).append(":type=").append(NODE_TYPE).
+                            append(",").append("name=").append(candidate.getName());
+                    ObjectName objName = new ObjectName(objNameBuilder.toString());
+                    mbeanServer.registerMBean(candidate, objName);
+                }
+            }
+            perClusterNodes.put(cluster.getName(), newMNodes);
         }
     }
 
@@ -77,6 +106,21 @@ public class JMXClusterMonitoring {
         @Override
         public String getStatus() {
             return status;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (obj != null && obj instanceof MCluster) {
+                MCluster other = (MCluster) obj;
+                return new EqualsBuilder().append(this.name, other.name).append(this.status, other.status).isEquals();
+            } else {
+                return false;
+            }
+        }
+
+        @Override
+        public int hashCode() {
+            return new HashCodeBuilder().append(name).append(status).toHashCode();
         }
 
         @Override
@@ -104,8 +148,48 @@ public class JMXClusterMonitoring {
         }
 
         @Override
+        public boolean equals(Object obj) {
+            if (obj != null && obj instanceof MNode) {
+                MNode other = (MNode) obj;
+                return new EqualsBuilder().append(this.name, other.name).append(this.host, other.host).isEquals();
+            } else {
+                return false;
+            }
+        }
+
+        @Override
+        public int hashCode() {
+            return new HashCodeBuilder().append(name).append(host).toHashCode();
+        }
+
+        @Override
         public String toString() {
             return new ToStringBuilder(this).append(name).append(host).toString();
+        }
+    }
+
+    private static class ClusterToMClusterTransformer implements Function<Cluster, MCluster> {
+
+        private final Router router;
+
+        public ClusterToMClusterTransformer(Router router) {
+            this.router = router;
+        }
+
+        @Override
+        public MCluster apply(Cluster source) {
+            Set<Node> nodes = router.clusterRoute(source);
+            String name = source.getName();
+            String status = nodes.isEmpty() ? MClusterMXBean.Status.UNAVAILABLE.toString() : MClusterMXBean.Status.AVAILABLE.toString();
+            return new MCluster(name, status);
+        }
+    }
+
+    private static class NodeToMNodeTransformer implements Function<Node, MNode> {
+
+        @Override
+        public MNode apply(Node source) {
+            return new MNode(source.getName(), source.getHost());
         }
     }
 }
