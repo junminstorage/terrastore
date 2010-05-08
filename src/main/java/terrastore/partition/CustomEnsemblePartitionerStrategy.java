8f9b13595dfdfcb2581ebf202d08954ad97780fa
sergio bossa <sergio.bossa@gmail.com>
2010-05-08 17:49:18 +0200

    Added some javadocs.

--- src/main/java/terrastore/partition/CustomEnsemblePartitionerStrategy.java
+++ src/main/java/terrastore/partition/CustomEnsemblePartitionerStrategy.java
@@ -1,14 +1,33 @@
 package terrastore.partition;
 
 /**
+ * Implement this interface to provide a custom partitioning strategy for distributing
+ * bucket and documents among ensemble clusters.
+ *
  * @author Sergio Bossa
  */
 public interface CustomEnsemblePartitionerStrategy {
 
+    /**
+     * Get the {@link Cluster} object where to place the given bucket.
+     *
+     * @param bucket The bucket to place.
+     * @return The {@link Cluster} object where to place the given bucket.
+     */
     public Cluster getClusterFor(String bucket);
 
+    /**
+     * Get the {@link Cluster} object where to place the given key (under the given bucket).
+     *
+     * @param bucket The bucket holding the key to place.
+     * @param key The key to place
+     * @return The {@link Cluster} object where to place the given key.
+     */
     public Cluster getClusterFor(String bucket, String key);
 
+    /**
+     * Represents a specific cluster where to place buckets and documents.
+     */
     public static class Cluster {
 
         private final String name;
@@ -17,6 +36,11 @@ public interface CustomEnsemblePartitionerStrategy {
             this.name = name;
         }
 
+        /**
+         * Get the unique cluster name.
+         * 
+         * @return the cluster name.
+         */
         public String getName() {
             return name;
         }
