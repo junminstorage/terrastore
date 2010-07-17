fd656032024d4d5a1fa560afdfe0a8e2a0340997
sergio bossa <sergio.bossa@gmail.com>
2010-07-17 20:09:11 +0200

    Implemented disk storage of sorted snapshots, based on HawtDB.

--- src/main/java/terrastore/store/SortedSnapshot.java
+++ src/main/java/terrastore/store/SortedSnapshot.java
@@ -15,11 +15,23 @@
  */
 package terrastore.store;
 
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.File;
+import java.io.IOException;
 import java.util.Comparator;
-import java.util.NavigableSet;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.Map.Entry;
 import java.util.Set;
-import java.util.TreeSet;
-import terrastore.util.collect.Sets;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+import org.fusesource.hawtbuf.codec.Codec;
+import org.fusesource.hawtbuf.codec.StringCodec;
+import org.fusesource.hawtdb.api.BTreeIndexFactory;
+import org.fusesource.hawtdb.api.PageFileFactory;
+import org.fusesource.hawtdb.api.Predicate;
+import org.fusesource.hawtdb.api.SortedIndex;
 
 /**
  * Sorted snapshot of keys.
@@ -28,27 +40,163 @@ import terrastore.util.collect.Sets;
  */
 public class SortedSnapshot {
 
-    private final NavigableSet<String> sortedKeys;
-    private final long timestamp;
+    private final ReadWriteLock stateLock;
+    private final Comparator<String> comparator;
+    private PageFileFactory pageFactory;
+    private BTreeIndexFactory<String, String> indexFactory;
+    private long timestamp;
 
-    public SortedSnapshot(Set<String> keys, Comparator<String> comparator) {
-        this.sortedKeys = new TreeSet<String>(comparator);
-        this.timestamp = System.currentTimeMillis();
-        this.sortedKeys.addAll(keys);
+    public SortedSnapshot(String name, Set<String> keys, Comparator<String> comparator) {
+        this.stateLock = new ReentrantReadWriteLock();
+        this.comparator = comparator;
+        computeIndex(getFile(name), keys, comparator);
     }
 
     public Set<String> keysInRange(String start, String end, int limit) {
-        NavigableSet<String> subSet = null;
-        if (end != null) {
-            subSet = sortedKeys.subSet(start, true, end, true);
-        } else {
-            subSet = sortedKeys.tailSet(start, true);
+        stateLock.readLock().lock();
+        try {
+            return queryIndex(start, end, limit);
+        } finally {
+            stateLock.readLock().unlock();
         }
-        return Sets.limited(subSet, limit);
     }
 
     public boolean isExpired(long timeToLive) {
-        long now = System.currentTimeMillis();
-        return (now - timestamp) >= timeToLive;
+        stateLock.readLock().lock();
+        try {
+            long now = System.currentTimeMillis();
+            return (now - timestamp) >= timeToLive;
+        } finally {
+            stateLock.readLock().unlock();
+        }
+    }
+
+    public void update(Set<String> keys) {
+        stateLock.writeLock().lock();
+        try {
+            recomputeIndex(keys);
+        } finally {
+            stateLock.writeLock().unlock();
+        }
+    }
+
+    public void discard() {
+        stateLock.writeLock().lock();
+        try {
+            pageFactory.close();
+        } finally {
+            stateLock.writeLock().unlock();
+        }
+    }
+
+    private File getFile(String name) {
+        try {
+            return File.createTempFile("terrastore", name);
+        } catch (IOException ex) {
+            throw new IllegalStateException("Unable to create snapshot file!");
+        }
+    }
+
+    private void computeIndex(File file, Set<String> keys, Comparator<String> comparator) {
+        pageFactory = new PageFileFactory();
+        pageFactory.setPageSize((short) 512);
+        pageFactory.setFile(file);
+        pageFactory.open();
+
+        indexFactory = new BTreeIndexFactory<String, String>();
+        indexFactory.setComparator(comparator);
+        indexFactory.setKeyCodec(StringCodec.INSTANCE);
+        indexFactory.setValueCodec(new Codec<String>() {
+
+            @Override
+            public void encode(String object, DataOutput dataOut) throws IOException {
+            }
+
+            @Override
+            public String decode(DataInput dataIn) throws IOException {
+                return "";
+            }
+
+            @Override
+            public int getFixedSize() {
+                return 0;
+            }
+
+            @Override
+            public int estimatedSize(String object) {
+                return 0;
+            }
+
+            @Override
+            public boolean isDeepCopySupported() {
+                return true;
+            }
+
+            @Override
+            public String deepCopy(String source) {
+                return "";
+            }
+
+            @Override
+            public boolean isEstimatedSizeSupported() {
+                return true;
+            }
+        });
+
+        SortedIndex<String, String> index = indexFactory.create(pageFactory.getPageFile());
+        for (String key : keys) {
+            index.put(key, "");
+        }
+        pageFactory.getPageFile().flush();
+
+        timestamp = System.currentTimeMillis();
+    }
+
+    private void recomputeIndex(final Set<String> keys) {
+        SortedIndex<String, String> index = indexFactory.open(pageFactory.getPageFile());
+        // Put new keys:
+        for (String key : keys) {
+            index.putIfAbsent(key, "");
+        }
+        // Remove old keys still in index:
+        int surplus = index.size() - keys.size();
+        if (surplus > 0) {
+            Iterator<Entry<String, String>> iterator = index.iterator(new Predicate<String>() {
+
+                @Override
+                public boolean isInterestedInKeysBetween(String first, String second, Comparator comparator) {
+                    return true;
+                }
+
+                @Override
+                public boolean isInterestedInKey(String key, Comparator comparator) {
+                    return !keys.contains(key);
+                }
+            });
+            while (iterator.hasNext() && surplus-- > 0) {
+                Entry<String, String> entry = iterator.next();
+                index.remove(entry.getKey());
+            }
+        }
+        //
+        pageFactory.getPageFile().flush();
+
+        timestamp = System.currentTimeMillis();
+    }
+
+    private Set<String> queryIndex(String start, String end, int limit) {
+        SortedIndex<String, String> index = indexFactory.open(pageFactory.getPageFile());
+        LinkedHashSet<String> result = new LinkedHashSet<String>();
+        Iterator<Entry<String, String>> entries = index.iterator(start);
+        int counter = 1;
+        while (entries.hasNext()) {
+            Entry<String, String> entry = entries.next();
+            if ((end == null || comparator.compare(entry.getKey(), end) <= 0) && (limit == 0 || counter++ <= limit)) {
+                result.add(entry.getKey());
+            } else {
+                break;
+            }
+        }
+        return result;
     }
 }
--- src/test/java/terrastore/store/SortedSnapshotTest.java
+++ src/test/java/terrastore/store/SortedSnapshotTest.java
@@ -18,6 +18,7 @@ package terrastore.store;
 import java.util.Comparator;
 import java.util.HashSet;
 import java.util.Set;
+import org.apache.commons.lang.time.StopWatch;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -34,11 +35,12 @@ public class SortedSnapshotTest {
         keys.add("c");
         keys.add("b");
 
-        SortedSnapshot snapshot = new SortedSnapshot(keys, new StringComparator());
+        SortedSnapshot snapshot = new SortedSnapshot("bucket", keys, new StringComparator());
         Set<String> sorted = snapshot.keysInRange("b", "c", 0);
         assertEquals(2, sorted.size());
         assertEquals("b", sorted.toArray()[0]);
         assertEquals("c", sorted.toArray()[1]);
+        snapshot.discard();
     }
 
     @Test
@@ -49,11 +51,12 @@ public class SortedSnapshotTest {
         keys.add("c");
         keys.add("b");
 
-        SortedSnapshot snapshot = new SortedSnapshot(keys, new StringComparator());
+        SortedSnapshot snapshot = new SortedSnapshot("bucket", keys, new StringComparator());
         Set<String> sorted = snapshot.keysInRange("c", null, 0);
         assertEquals(2, sorted.size());
         assertEquals("c", sorted.toArray()[0]);
         assertEquals("v", sorted.toArray()[1]);
+        snapshot.discard();
     }
 
     @Test
@@ -64,29 +67,183 @@ public class SortedSnapshotTest {
         keys.add("c");
         keys.add("b");
 
-        SortedSnapshot snapshot = new SortedSnapshot(keys, new StringComparator());
+        SortedSnapshot snapshot = new SortedSnapshot("bucket", keys, new StringComparator());
         Set<String> sorted = snapshot.keysInRange("a", "c", 2);
         assertEquals(2, sorted.size());
         assertEquals("a", sorted.toArray()[0]);
         assertEquals("b", sorted.toArray()[1]);
+        snapshot.discard();
+    }
+
+    @Test
+    public void testUpdateWithMoreKeys() {
+        Set<String> keys = new HashSet<String>();
+        keys.add("d");
+        keys.add("a");
+        keys.add("c");
+        keys.add("b");
+
+        SortedSnapshot snapshot = new SortedSnapshot("bucket", keys, new StringComparator());
+        Set<String> sorted = snapshot.keysInRange("a", "z", 0);
+        assertEquals(4, sorted.size());
+
+        keys.add("e");
+        snapshot.update(keys);
+        sorted = snapshot.keysInRange("a", "z", 0);
+        assertEquals(5, sorted.size());
+        assertEquals("a", sorted.toArray()[0]);
+        assertEquals("b", sorted.toArray()[1]);
+        assertEquals("c", sorted.toArray()[2]);
+        assertEquals("d", sorted.toArray()[3]);
+        assertEquals("e", sorted.toArray()[4]);
+
+        snapshot.discard();
+    }
+
+    @Test
+    public void testUpdateWithLessKeys() {
+        Set<String> keys = new HashSet<String>();
+        keys.add("d");
+        keys.add("a");
+        keys.add("c");
+        keys.add("b");
+
+        SortedSnapshot snapshot = new SortedSnapshot("bucket", keys, new StringComparator());
+        Set<String> sorted = snapshot.keysInRange("a", "z", 0);
+        assertEquals(4, sorted.size());
+
+        keys.remove("d");
+        snapshot.update(keys);
+        sorted = snapshot.keysInRange("a", "z", 0);
+        assertEquals(3, sorted.size());
+        assertEquals("a", sorted.toArray()[0]);
+        assertEquals("b", sorted.toArray()[1]);
+        assertEquals("c", sorted.toArray()[2]);
+
+        snapshot.discard();
+    }
+
+    @Test
+    public void testUpdateWithEqualNumberButDifferentKeys() {
+        Set<String> keys = new HashSet<String>();
+        keys.add("d");
+        keys.add("a");
+        keys.add("c");
+        keys.add("b");
+
+        SortedSnapshot snapshot = new SortedSnapshot("bucket", keys, new StringComparator());
+        Set<String> sorted = snapshot.keysInRange("a", "z", 0);
+        assertEquals(4, sorted.size());
+
+        keys.remove("a");
+        keys.remove("b");
+        keys.remove("c");
+        keys.remove("d");
+        keys.add("e");
+        keys.add("f");
+        keys.add("g");
+        keys.add("h");
+        snapshot.update(keys);
+        sorted = snapshot.keysInRange("a", "z", 0);
+        assertEquals(4, sorted.size());
+        assertEquals("e", sorted.toArray()[0]);
+        assertEquals("f", sorted.toArray()[1]);
+        assertEquals("g", sorted.toArray()[2]);
+        assertEquals("h", sorted.toArray()[3]);
+
+        snapshot.discard();
+    }
+
+    @Test
+    public void testUpdateWithDifferentNumberAndDifferentKeys() {
+        Set<String> keys = new HashSet<String>();
+        keys.add("d");
+        keys.add("a");
+        keys.add("c");
+        keys.add("b");
+
+        SortedSnapshot snapshot = new SortedSnapshot("bucket", keys, new StringComparator());
+        Set<String> sorted = snapshot.keysInRange("a", "z", 0);
+        assertEquals(4, sorted.size());
+
+        keys.remove("a");
+        keys.remove("b");
+        keys.remove("c");
+        keys.remove("d");
+        keys.add("z");
+        snapshot.update(keys);
+        sorted = snapshot.keysInRange("a", "z", 0);
+        assertEquals(1, sorted.size());
+        assertEquals("z", sorted.toArray()[0]);
+
+        snapshot.discard();
     }
 
     @Test
     public void testIsExpired() throws InterruptedException {
         Set<String> keys = new HashSet<String>();
 
-        SortedSnapshot snapshot = new SortedSnapshot(keys, new StringComparator());
+        SortedSnapshot snapshot = new SortedSnapshot("bucket", keys, new StringComparator());
         Thread.sleep(1000);
         assertTrue(snapshot.isExpired(500));
+        snapshot.discard();
     }
 
     @Test
     public void testIsNotExpired() throws InterruptedException {
         Set<String> keys = new HashSet<String>();
 
-        SortedSnapshot snapshot = new SortedSnapshot(keys, new StringComparator());
+        SortedSnapshot snapshot = new SortedSnapshot("bucket", keys, new StringComparator());
         Thread.sleep(1000);
         assertFalse(snapshot.isExpired(5000));
+        snapshot.discard();
+    }
+
+    @Test
+    public void testPerf() {
+        Set<String> keys = new HashSet<String>();
+        int total = 300000;
+
+        for (int i = 0; i < total; i++) {
+            keys.add("" + i);
+        }
+
+        StopWatch indexing = new StopWatch();
+        indexing.start();
+        SortedSnapshot snapshot = new SortedSnapshot("bucket", keys, new LongStringComparator());
+        indexing.stop();
+        System.out.println("Indexing: " + indexing.getTime());
+
+        StopWatch querying = new StopWatch();
+        querying.start();
+        Set<String> sorted = snapshot.keysInRange("100", "102", 2);
+        querying.stop();
+        assertEquals(2, sorted.size());
+        assertEquals("100", sorted.toArray()[0]);
+        assertEquals("101", sorted.toArray()[1]);
+        System.out.println("Querying: " + querying.getTime());
+
+        for (int i = 10; i < 110; i++) {
+            keys.remove("" + i);
+        }
+        for (int i = total; i < total + 10000; i++) {
+            keys.add("" + i);
+        }
+
+        StopWatch updating = new StopWatch();
+        updating.start();
+        snapshot.update(keys);
+        updating.stop();
+        System.out.println("Updating: " + updating.getTime());
+
+        StopWatch querying2 = new StopWatch();
+        querying2.start();
+        sorted = snapshot.keysInRange("100", "102", 2);
+        querying2.stop();
+        assertEquals(0, sorted.size());
+        System.out.println("Querying 2: " + querying2.getTime());
+
+        snapshot.discard();
     }
 
     private static class StringComparator implements Comparator<String> {
@@ -94,6 +251,14 @@ public class SortedSnapshotTest {
         @Override
         public int compare(String o1, String o2) {
             return o1.compareTo(o2);
+        }
+    }
+
+    private static class LongStringComparator implements Comparator<String> {
+
+        @Override
+        public int compare(String o1, String o2) {
+            return Long.valueOf(o1).compareTo(Long.valueOf(o2));
         }
     }
 }
