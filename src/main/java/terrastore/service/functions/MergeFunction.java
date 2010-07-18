fa873af1f6258df50ed36aa1870244b31788bc8d
johansson.sven <johansson.sven@localhost>
2010-07-18 20:29:41 +0200

    Added MergeFunction update function.

---
+++ src/main/java/terrastore/service/functions/MergeFunction.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright 2009 - 2010 Sergio Bossa (sergio.bossa@gmail.com)
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package terrastore.service.functions;
+
+import java.util.Map;
+
+import terrastore.store.operators.Function;
+
+/**
+ * @author Sven Johansson
+ */
+@SuppressWarnings("serial")
+public class MergeFunction implements Function {
+
+    @Override
+    public Map<String, Object> apply(String key, Map<String, Object> value, Map<String, Object> parameters) {
+        value.putAll(parameters);
+        return value;
+    }
+
+}
