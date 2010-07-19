d350f73767b589f496e8ea7138ef84dd3c8ebb77
johansson.sven <johansson.sven@localhost>
2010-07-19 22:53:13 +0200

    MergeFunction update - Merge on embedded objects no longer destroy original fields not referenced.

--- src/main/java/terrastore/service/functions/MergeFunction.java
+++ src/main/java/terrastore/service/functions/MergeFunction.java
@@ -16,7 +16,9 @@
 
 package terrastore.service.functions;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import terrastore.store.operators.Function;
 
@@ -28,8 +30,43 @@ public class MergeFunction implements Function {
 
     @Override
     public Map<String, Object> apply(String key, Map<String, Object> value, Map<String, Object> parameters) {
-        value.putAll(parameters);
-        return value;
+        return copyFields(value, parameters);
+    }
+    
+    private Map<String, Object> copyFields(Map<String, Object> value, Map<String, Object> newValues) {
+        Map<String, Object> result = getMap(value);
+        
+        for (Entry<String, Object> entry : newValues.entrySet()) {
+            if (isMap(entry) && isMap(value.get(entry.getKey()))) {
+                copyEmbeddedFields(value, entry);
+            } else {
+                result.put(entry.getKey(), entry.getValue());
+            }
+        }
+        
+        return result;
+    }
+    
+    private void copyEmbeddedFields(Map<String, Object> value, Entry<String, Object> entry) {
+        Map<String, Object> embeddedValue = (Map<String, Object>) value.get(entry.getKey());
+        Map<String, Object> embeddedNewValues = (Map<String, Object>) entry.getValue();
+        copyFields(embeddedValue, embeddedNewValues);
     }
 
+    private boolean isMap(Entry<String, Object> counterEntry) {
+        return Map.class.isAssignableFrom(counterEntry.getValue().getClass());    
+    }
+    
+    private boolean isMap(Object object) {
+        return object != null && Map.class.isAssignableFrom(object.getClass());
+    }
+
+    private Map<String, Object> getMap(Map<String, Object> map) {
+        if (null == map) {
+            return new HashMap<String, Object>();
+        }
+        return map;
+    }
+
+    
 }
