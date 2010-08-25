458b786a9911a23feadb64ea564eaf7d3ce5ed52
sergio bossa <sergio.bossa@gmail.com>
2010-08-25 17:48:45 +0200

    Improved CORS implementation to support multiple, command-line configurable, allowed origins.

--- src/main/java/terrastore/server/impl/cors/CorsController.java
+++ src/main/java/terrastore/server/impl/cors/CorsController.java
@@ -1,38 +1,40 @@
-package terrastore.server.impl;
+package terrastore.server.impl.cors;
 
 import javax.ws.rs.OPTIONS;
 import javax.ws.rs.Path;
+
 /**
- * This class is a Cross Origin Resource Sharing implementation,<br>
- * it allows the preflight request and a CORS mechanism for not base methods(GET, POST) come true.
+ * This class is a Cross Origin Resource Sharing implementation.<br>
  * For more details see <a href="http://www.w3.org/TR/access-control/">CORS specification</a>.
- * @author Giuseppe Santoro
  *
+ * @author Giuseppe Santoro
  */
 @Path("/")
 public class CorsController {
 
+    private static final String OK = "ok";
+
     @OPTIONS
     @Path("/{bucket}/{key}/{operation}")
     public String corsThreeVars() {
-        return "ok";
+        return OK;
     }
 
     @OPTIONS
     @Path("/{bucket}/{key}")
     public String corsTwoVars() {
-        return "ok";
+        return OK;
     }
 
     @OPTIONS
     @Path("/{bucket}")
     public String corsOneVar() {
-        return "ok";
+        return OK;
     }
 
     @OPTIONS
     @Path("/")
     public String corsZeroVar() {
-        return "ok";
+        return OK;
     }
 }
