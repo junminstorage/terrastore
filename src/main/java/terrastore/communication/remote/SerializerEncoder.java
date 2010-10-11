281f3ca33650fcf7f52c8f5df64d7b92665ce095
sergio bossa <sergio.bossa@gmail.com>
2010-10-11 15:21:52 +0200

    Removed deprecated Netty annotations.

--- src/main/java/terrastore/communication/remote/SerializerEncoder.java
+++ src/main/java/terrastore/communication/remote/SerializerEncoder.java
@@ -17,15 +17,15 @@ package terrastore.communication.remote;
 
 import org.jboss.netty.buffer.ChannelBuffers;
 import org.jboss.netty.channel.Channel;
+import org.jboss.netty.channel.ChannelHandler.Sharable;
 import org.jboss.netty.channel.ChannelHandlerContext;
-import org.jboss.netty.channel.ChannelPipelineCoverage;
 import org.jboss.netty.handler.codec.oneone.OneToOneEncoder;
 import terrastore.util.io.Serializer;
 
 /**
  * @author Sergio Bossa
  */
-@ChannelPipelineCoverage("all")
+@Sharable
 public class SerializerEncoder<T> extends OneToOneEncoder {
 
     private final Serializer<T> serializer;
@@ -38,4 +38,5 @@ public class SerializerEncoder<T> extends OneToOneEncoder {
     protected Object encode(ChannelHandlerContext ctx, Channel channel, Object msg) throws Exception {
         return ChannelBuffers.wrappedBuffer(serializer.serialize((T) msg));
     }
+
 }
