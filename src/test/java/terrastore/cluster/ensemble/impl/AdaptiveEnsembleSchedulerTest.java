caf7ae82019f18da0ba45bd07e32a6cb47731aa4
Amir Moulavi <amir.moulavi@gmail.com>
2010-10-20 21:46:32 +0200

    A new test is added to cover the 0 delay case for Scheduler

--- src/test/java/terrastore/cluster/ensemble/impl/AdaptiveEnsembleSchedulerTest.java
+++ src/test/java/terrastore/cluster/ensemble/impl/AdaptiveEnsembleSchedulerTest.java
@@ -21,10 +21,13 @@ import static org.easymock.classextension.EasyMock.makeThreadSafe;
 import static org.easymock.classextension.EasyMock.replay;
 import static org.easymock.classextension.EasyMock.verify;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import terrastore.cluster.ensemble.EnsembleConfiguration;
 import terrastore.cluster.ensemble.EnsembleManager;
+import terrastore.cluster.ensemble.EnsembleConfiguration.DiscoveryConfiguration;
 import terrastore.communication.Cluster;
 
 /**
@@ -34,30 +37,58 @@ import terrastore.communication.Cluster;
  */
 public class AdaptiveEnsembleSchedulerTest {
 
+    private Cluster cluster;
+    private FuzzyInferenceEngine fuzzy;
+    private View view;
+    private DiscoveryConfiguration discoveryConfiguration;
+    private EnsembleConfiguration ensembleConfiguration;
+    private EnsembleManager ensemble;
+    private AdaptiveEnsembleScheduler scheduler;
+
+    @Before
+    public void set_up() {
+        cluster = new Cluster("cluster", false);
+        scheduler = new AdaptiveEnsembleScheduler(fuzzy);
+    }
+    
     @Test
-    public void testSchedule() throws Exception {
-        Cluster cluster = new Cluster("cluster", false);
+    public void scheduler_works() throws Exception {
+        given(
+                fuzzy_inference_engine(), 
+                view(), 
+                discovery_configuration(),
+                ensemble_configuration(),
+                ensemble_manager()
+             );
+        
+        when_all_objects_are_constructed_and_called_properly();
 
-        FuzzyInferenceEngine fuzzy = createMock(FuzzyInferenceEngine.class);
-        makeThreadSafe(fuzzy, true);
-        View view = createMock(View.class);
-        makeThreadSafe(view, true);
-        EnsembleConfiguration.DiscoveryConfiguration discoveryConfiguration = createMock(EnsembleConfiguration.DiscoveryConfiguration.class);
-        makeThreadSafe(discoveryConfiguration, true);
-        discoveryConfiguration.getInterval();
-        expectLastCall().andReturn(3000L).once();
-        EnsembleConfiguration ensembleConfiguration = createMock(EnsembleConfiguration.class);
-        makeThreadSafe(ensembleConfiguration, true);
-        ensembleConfiguration.getDiscovery();
-        expectLastCall().andReturn(discoveryConfiguration).once();
-        EnsembleManager ensemble = createMock(EnsembleManager.class);
-        makeThreadSafe(ensemble, true);
-        ensemble.update(cluster);
-        expectLastCall().andReturn(view).once();
+        then_scheduler_works_as_expected();
+    }
 
+    @Test
+    public void scheduler_starts_with_a_delay() throws Exception {
+        given(
+                fuzzy_inference_engine(), 
+                view(), 
+                discovery_configuration(),
+                ensemble_configuration()
+             );
+        
+        when_all_objects_are_constructed_and_partially_called();
+        
+        then_scheduler_should_not_be_terminated_after_one_second();
+    }
+    
+    private void when_all_objects_are_constructed_and_partially_called() {
+        replay(fuzzy, discoveryConfiguration, ensembleConfiguration);
+    }
+
+    private void when_all_objects_are_constructed_and_called_properly() throws Exception {
         replay(fuzzy, discoveryConfiguration, ensembleConfiguration, ensemble);
+    }
 
-        AdaptiveEnsembleScheduler scheduler = new AdaptiveEnsembleScheduler(fuzzy);
+    private void then_scheduler_works_as_expected() {
         try {
             scheduler.schedule(cluster, ensemble, ensembleConfiguration);
             Thread.sleep(4000);
@@ -65,8 +96,66 @@ public class AdaptiveEnsembleSchedulerTest {
             ex.printStackTrace();
         } finally {
             scheduler.shutdown();
-            verify(fuzzy, discoveryConfiguration, ensembleConfiguration, ensemble);
+            verify(fuzzy, discoveryConfiguration, ensembleConfiguration);
         }
+    }
+
+    private void then_scheduler_should_not_be_terminated_after_one_second() {
+        try {
+            scheduler.schedule(cluster, ensemble, ensembleConfiguration);
+            Thread.sleep(1000);
+        } catch (Throwable ex) {
+            ex.printStackTrace();
+        } finally {
+            Assert.assertFalse("Scheduler should not be terminated but it is terminated. Have you changed the delay back to 0?!", scheduler.getScheduler().isTerminated());
+            scheduler.shutdown();
+            verify(fuzzy, discoveryConfiguration, ensembleConfiguration);
+        }
+    }
+
+
+    private void given(Object... object) {
+    }
+
+    private EnsembleManager ensemble_manager() throws Exception {
+        EnsembleManager ensemble = createMock(EnsembleManager.class);
+        makeThreadSafe(ensemble, true);
+        this.ensemble = ensemble;
+        ensemble.update(cluster);
+        expectLastCall().andReturn(view).once();
+        return ensemble;
+    }
+
+    private EnsembleConfiguration ensemble_configuration() {
+        EnsembleConfiguration ensembleConfiguration = createMock(EnsembleConfiguration.class);
+        makeThreadSafe(ensembleConfiguration, true);
+        this.ensembleConfiguration = ensembleConfiguration;
+        ensembleConfiguration.getDiscovery();
+        expectLastCall().andReturn(discoveryConfiguration).once();
+        return ensembleConfiguration;
+    }
+
+    private EnsembleConfiguration.DiscoveryConfiguration discovery_configuration() {
+        EnsembleConfiguration.DiscoveryConfiguration discoveryConfiguration = createMock(EnsembleConfiguration.DiscoveryConfiguration.class);
+        makeThreadSafe(discoveryConfiguration, true);
+        this.discoveryConfiguration = discoveryConfiguration;
+        discoveryConfiguration.getInterval();
+        expectLastCall().andReturn(3000L).once();
+        return discoveryConfiguration;
+    }
+
+    private View view() {
+        View view = createMock(View.class);
+        makeThreadSafe(view, true);
+        this.view = view;
+        return view;
+    }
+
+    private FuzzyInferenceEngine fuzzy_inference_engine() {
+        FuzzyInferenceEngine fuzzy = createMock(FuzzyInferenceEngine.class);
+        makeThreadSafe(fuzzy, true);
+        this.fuzzy = fuzzy;
+        return fuzzy;
     }
 
 }
