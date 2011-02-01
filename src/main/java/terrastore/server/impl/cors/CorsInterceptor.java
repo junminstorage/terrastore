d1c692856024be0e06156090709d372ab8fba5f3
sergio bossa <sergio.bossa@gmail.com>
2011-02-01 08:34:49 +0100

    Updated license headers.

--- src/main/java/terrastore/server/impl/cors/CorsInterceptor.java
+++ src/main/java/terrastore/server/impl/cors/CorsInterceptor.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2009 - 2010 Sergio Bossa (sergio.bossa@gmail.com)
+ * Copyright 2009 - 2011 Sergio Bossa (sergio.bossa@gmail.com)
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
